c362b07b4043cf47189b54bb7089d0e6f51fbcac
Lajaynees <lscingram@yahoo.com>
2021-11-01 18:36:24 -0400

    added engineer/intern prompts

--- index.js
+++ index.js
@@ -33,10 +33,70 @@ const initial = () => {
             name: "managerOfficeNumber"
         }
     ]).then(response => {
-        const newManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber)
+        const newManager = newManager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber)
         employee.push(newManager)
         addTeam();
     })
+}
+
+//copy paste for engineer and intern prompts
+const newEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the engineer?",
+            name: "engineerName"
+        },
+        {
+            type: "input",
+            message: "What is the id number of the engineer?",
+            name: "engineerId"
+        },
+        {
+            type: "input",
+            message: "What is the email address of the engineer?",
+            name: "engineerEmail"
+        },
+        {
+            type: "input",
+            message: "What is the GitHub username of the engineer?",
+            name: "engineerGithub"
+        }
+    ]).then(response => {
+        const newEngineer = newEngineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub)
+        employee.push(newEngineer);
+        addTeam();
+    })
+}
+
+const newIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the intern?",
+            name: "internName"
+        },
+        {
+            type: "input",
+            message: "What is the id number of the intern?",
+            name: "internId"
+        },
+        {
+            type: "input",
+            message: "What is the Email address of the intern?",
+            name: "internEmail"
+        },
+        {
+            type: "input",
+            message: "What school does the intern go to?",
+            name: "internSchool"
+        }
+    ]).then(response => {
+        const newIntern = newIntern(response.internName, response.internId, response.internEmail, response.internSchool)
+        employee.push(newIntern);
+        addTeam();
+      
+    });
 }
 
 
